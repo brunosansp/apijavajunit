077ebe48227e63e4e2ef84d3a1e2249508c02e0f
Bruno Santos <bruno.pinto@MJV.net>
2023-07-27 21:35:54 -0300

    Project created

---
+++ src/main/java/br/com/brunosansp/apijavajunit/ApijavajunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunosansp.apijavajunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijavajunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijavajunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunosansp/apijavajunit/ApijavajunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunosansp.apijavajunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijavajunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
