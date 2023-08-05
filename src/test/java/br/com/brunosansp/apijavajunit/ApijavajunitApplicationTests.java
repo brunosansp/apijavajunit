58ea4ef880238fb13196573d0482fea6182812b9
Bruno Santos <bruno.pinto@MJV.net>
2023-08-04 21:05:56 -0300

    implementation of test to main class

--- src/test/java/br/com/brunosansp/apijavajunit/ApijavajunitApplicationTests.java
+++ src/test/java/br/com/brunosansp/apijavajunit/ApijavajunitApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApijavajunitApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApijavajunitApplication.main(new String[] {});
 	}
 
 }
