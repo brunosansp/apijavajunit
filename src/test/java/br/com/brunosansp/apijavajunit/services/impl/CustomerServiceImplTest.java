4d97439630dbab8c2792be44897b41edff00fa3f
Bruno Santos <bruno.pinto@MJV.net>
2023-08-21 22:01:22 -0300

    implemented test for class customer method findAll

--- src/test/java/br/com/brunosansp/apijavajunit/services/impl/CustomerServiceImplTest.java
+++ src/test/java/br/com/brunosansp/apijavajunit/services/impl/CustomerServiceImplTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,6 +31,7 @@ class CustomerServiceImplTest {
   public static final String EMAIL = "teste@mail.com";
   public static final String PASSWORD = "123";
   public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+  public static final int INDEX = 0;
   
   @InjectMocks
   private CustomerServiceImpl service;
@@ -83,9 +85,20 @@ class CustomerServiceImplTest {
   }
 
   @Test
-  void findAll() {
+  void whenFindAllThenReturnAnListOfCustomers() {
+    when(repository.findAll()).thenReturn(List.of(customer));
+    List<Customer> response = service.findAll();
+    
+    assertNotNull(response);
+    assertEquals(1, response.size());
+    assertEquals(Customer.class, response.get(INDEX).getClass());
+    
+    assertEquals(ID, response.get(INDEX).getId());
+    assertEquals(NAME, response.get(INDEX).getName());
+    assertEquals(EMAIL, response.get(INDEX).getEmail());
+    assertEquals(PASSWORD, response.get(INDEX).getPassword());
   }
-
+  
   @Test
   void create() {
   }
